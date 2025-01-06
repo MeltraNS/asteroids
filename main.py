9b5a44a52303b6dc8453e0997b53c3cf0fe77e90
Meltra <sgreaves7500@gmail.com>
2025-01-06 17:39:42 -0500

    Can move cursor

--- main.py
+++ main.py
@@ -7,15 +7,23 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    x = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
-        x = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        x.draw(screen)
+        #  x.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+        screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
+        # x.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
--- player.py
+++ player.py
@@ -17,3 +17,22 @@ class Player(CircleShape):  # create a class named Player that inherits from Cir
     
     def draw(self, screen):  # define a function draw, accepts the screen object as an agruments, also overrides the CircleShape draw method
         pygame.draw.polygon(screen, "white", self.triangle() , 2)  # on the screen, draw a white triangle with a line width of 2
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)           
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
