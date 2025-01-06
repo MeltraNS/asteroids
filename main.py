b996ce4a92c75b2bdadf4ffac5b02ef3a3e2aae3
Meltra <sgreaves7500@gmail.com>
2025-01-06 16:30:27 -0500

    Drew a triangle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -12,6 +14,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        x = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        x.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
