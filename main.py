e5ed9984c3e9c83d17d89028a3ab2acaea939f06
Meltra <sgreaves7500@gmail.com>
2025-01-07 16:58:09 -0500

    Added shots

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     AsteroidField.containers = updatable
-    
+
     x = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     y = AsteroidField()
     
--- player.py
+++ player.py
@@ -18,6 +18,9 @@ class Player(CircleShape):  # create a class named Player that inherits from Cir
     def draw(self, screen):  # define a function draw, accepts the screen object as an agruments, also overrides the CircleShape draw method
         pygame.draw.polygon(screen, "white", self.triangle() , 2)  # on the screen, draw a white triangle with a line width of 2
 
+    def shoot(self, dt):
+        z = Shot(self.x, self.
+        
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
@@ -32,6 +35,8 @@ class Player(CircleShape):  # create a class named Player that inherits from Cir
             self.move(dt)           
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt)
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
